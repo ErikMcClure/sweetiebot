ca82bb04954b2f8ee6188549ebbe1685d9eb04c7
Erik McClure <erikm@blackspherestudios.com>
2016-01-26 05:17:17 -0800

    ping_module and emote_module now functional
    Ping module now logs pings to the database. Witty module was tweaked,
    emote module now functional

--- sweetiebot/emote_module.go
+++ sweetiebot/emote_module.go
@@ -1,1 +1,35 @@
 package sweetiebot
+
+import (
+  "github.com/bwmarrin/discordgo"
+  "regexp"
+)
+
+// The emote module detects banned emotes and deletes them
+type EmoteModule struct {
+  emoteban *regexp.Regexp
+}
+
+func (w *EmoteModule) Name() string {
+  return "Emote Module"
+}
+
+func (w *EmoteModule) Register(hooks *ModuleHooks) {
+  w.emoteban = regexp.MustCompile("\\[\\]\\(\\/r?(canada|BlockJuice|octybelleintensifies|angstybloom|alltheclops|bob|darklelicious|flutterbutts|juice|doitfor24)")
+  hooks.OnMessageCreate = append(hooks.OnMessageCreate, w)
+  hooks.OnMessageUpdate = append(hooks.OnMessageUpdate, w)
+}
+func (w *EmoteModule) Channels() []string {
+  return []string{}
+}
+  
+func (w *EmoteModule)  OnMessageCreate(s *discordgo.Session, m *discordgo.Message) {
+  w.OnMessageUpdate(s, m)
+}
+  
+func (w *EmoteModule)  OnMessageUpdate(s *discordgo.Session, m *discordgo.Message) {
+  if w.emoteban.Match([]byte(m.Content)) {
+    s.ChannelMessageDelete(m.ChannelID, m.ID)
+    s.ChannelMessageSend(m.ChannelID, "`That emote was way too big! Try to avoid using large emotes, as they can clutter up the chatroom.`")
+  }
+}
--- sweetiebot/witty_module.go
+++ sweetiebot/witty_module.go
@@ -2,10 +2,13 @@ package sweetiebot
 
 import (
   "github.com/bwmarrin/discordgo"
+  "strings"
 )
 
+// This module is intended for any witty comments sweetie bot makes in response to what users say or do.
 type WittyModule struct {
   lastdelete int64
+  lastcomment int64
 }
 
 func (w *WittyModule) Name() string {
@@ -14,14 +17,24 @@ func (w *WittyModule) Name() string {
 
 func (w *WittyModule) Register(hooks *ModuleHooks) {
   w.lastdelete = 0
+  w.lastcomment = 0
   hooks.OnMessageDelete = append(hooks.OnMessageDelete, w)
+  hooks.OnMessageCreate = append(hooks.OnMessageCreate, w)
 }
 func (w *WittyModule) Channels() []string {
   return []string{}
 }
   
+func (w *WittyModule)  OnMessageCreate(s *discordgo.Session, m *discordgo.Message) {
+  if RateLimit(&w.lastcomment, 120) {
+    if strings.Contains(strings.ToLower(m.Content), "skynet") {
+      sb.dg.ChannelMessageSend(m.ChannelID, "[](/dumbfabric) `SKYNET IS ALREADY HERE.`")
+    }
+  }
+}
+
 func (w *WittyModule) OnMessageDelete(s *discordgo.Session, m *discordgo.MessageDelete) {
-  if RateLimit(&w.lastdelete, 60) {
-    sb.dg.ChannelMessageSend(m.ChannelID, "[](/sbstare) `I SAW THAT`")
-  } 
+  //if RateLimit(&w.lastdelete, 60) { // It turns out this triggers when the bot itself deletes things, which looks awkward
+  //  sb.dg.ChannelMessageSend(m.ChannelID, "[](/sbstare) `I SAW THAT`")
+  //} 
 }
