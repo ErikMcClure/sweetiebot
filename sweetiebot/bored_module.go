572af3db4a40776a7cacf50d9ece1a593802eb0d
Erik McClure <erikm@fundament.software>
2016-02-02 05:13:44 -0800

    Version 0.3.3 commit
    Added transcript database, markov chain, quote command, genepisode
    command, and bored module outline (nonfunctional)

---
+++ sweetiebot/bored_module.go
@@ -0,0 +1,32 @@
+package sweetiebot
+
+import (
+  "github.com/bwmarrin/discordgo"
+)
+
+// This module picks a random action to do whenever #manechat has been idle for several minutes (configurable)
+type BoredModule struct {
+  ModuleEnabled
+  lastmessage int64 // Ensures discord screwing up doesn't make us spam the chatroom.
+}
+
+func (w *BoredModule) Name() string {
+  return "Bored"
+}
+
+func (w *BoredModule) Register(hooks *ModuleHooks) {
+  w.lastmessage = 0
+}
+func (w *BoredModule) Channels() []string {
+  return []string{"manechat"} // This doesn't really matter because OnIdle will only fire for the manechat.
+}
+ 
+func (w *BoredModule) OnIdle(s *discordgo.Session) {
+  q := &QuoteCommand{};
+  r, _ := q.Process([]string{"action"}, nil)
+  s.ChannelMessageSend(sb.ManeChannelID, r) // We pass in nil for the user because this particular function ignores it.
+}
+
+func (w *BoredModule) IdlePeriod() int64 {
+  return sb.config.Maxbored;
+}
