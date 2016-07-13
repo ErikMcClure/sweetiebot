a25524895fcb2eabd8ba1107a634cd6d7cc5dde5
Erik McClure <erikm@blackspherestudios.com>
2016-07-13 14:20:00 -0700

    Improve setconfig, reformat everything

--- sweetiebot/collections_command.go
+++ sweetiebot/collections_command.go
@@ -1,85 +1,86 @@
 package sweetiebot
 
 import (
-  "github.com/bwmarrin/discordgo"
-  "strings"
-  "strconv"
+	"strconv"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 type AddCommand struct {
-  funcmap map[string]func(string)string
+	funcmap map[string]func(string) string
 }
 
 func (c *AddCommand) Name() string {
-  return "Add";  
+	return "Add"
 }
-func (c *AddCommand) Process(args []string, msg *discordgo.Message, info *GuildInfo) (string, bool) {   
-  if len(args) < 1 {
-    return "```No collection given```", false
-  }
-  if len(args) < 2 {
-    return "```Can't add empty string!```", false
-  }
-  
-  collection := args[0]
-  _, ok := info.config.Collections[collection];
-  if !ok {
-    return "```That collection does not exist!```", false
-  }
+func (c *AddCommand) Process(args []string, msg *discordgo.Message, info *GuildInfo) (string, bool) {
+	if len(args) < 1 {
+		return "```No collection given```", false
+	}
+	if len(args) < 2 {
+		return "```Can't add empty string!```", false
+	}
 
-  arg := strings.Join(args[1:], " ")
-  info.config.Collections[collection][arg] = true
-  fn, ok := c.funcmap[collection];
-  retval := "```Added " + arg + " to " + collection + ". Length of " + collection + ": " + strconv.Itoa(len(info.config.Collections[collection])) + "```"
-  if ok {
-    retval = fn(arg)
-  }
-  info.SaveConfig()
-  return ExtraSanitize(retval), false
+	collection := args[0]
+	_, ok := info.config.Collections[collection]
+	if !ok {
+		return "```That collection does not exist!```", false
+	}
+
+	arg := strings.Join(args[1:], " ")
+	info.config.Collections[collection][arg] = true
+	fn, ok := c.funcmap[collection]
+	retval := "```Added " + arg + " to " + collection + ". Length of " + collection + ": " + strconv.Itoa(len(info.config.Collections[collection])) + "```"
+	if ok {
+		retval = fn(arg)
+	}
+	info.SaveConfig()
+	return ExtraSanitize(retval), false
 }
-func (c *AddCommand) Usage(info *GuildInfo) string { 
-  return info.FormatUsage(c, "[collection] [arbitrary string]", "Adds [arbitrary string] to [collection] (no quotes are required), then calls a handler function for that specific collection.") 
+func (c *AddCommand) Usage(info *GuildInfo) string {
+	return info.FormatUsage(c, "[collection] [arbitrary string]", "Adds [arbitrary string] to [collection] (no quotes are required), then calls a handler function for that specific collection.")
 }
 func (c *AddCommand) UsageShort() string { return "Adds a line to a collection." }
 
 type RemoveCommand struct {
-  funcmap map[string]func(string)string
+	funcmap map[string]func(string) string
 }
 
 func (c *RemoveCommand) Name() string {
-  return "Remove";  
+	return "Remove"
 }
-func (c *RemoveCommand) Process(args []string, msg *discordgo.Message, info *GuildInfo) (string, bool) {  
-  if len(args) < 1 {
-    return "```No collection given```", false
-  }
-  if len(args) < 2 {
-    return "```Can't remove an empty string!```", false
-  }
+func (c *RemoveCommand) Process(args []string, msg *discordgo.Message, info *GuildInfo) (string, bool) {
+	if len(args) < 1 {
+		return "```No collection given```", false
+	}
+	if len(args) < 2 {
+		return "```Can't remove an empty string!```", false
+	}
 
-  collection := args[0]
-  cmap, ok := info.config.Collections[collection];
-  if !ok {
-    return "```That collection does not exist!```", false
-  }
-  
-  arg := strings.Join(args[1:], " ")
-  _, ok = cmap[arg]
-  if !ok {
-    return "```Could not find " + arg + "!```", false
-  }
-  delete(info.config.Collections[collection], arg)
-  fn, ok := c.funcmap[collection];
-  retval := "```Removed " + arg + " from " + collection + ". Length of " + collection + ": " + strconv.Itoa(len(info.config.Collections[collection])) + "```"
-  if ok {
-    retval = fn(arg)
-  }
+	collection := args[0]
+	cmap, ok := info.config.Collections[collection]
+	if !ok {
+		return "```That collection does not exist!```", false
+	}
 
-  info.SaveConfig()
-  return ExtraSanitize(retval), false
+	arg := strings.Join(args[1:], " ")
+	_, ok = cmap[arg]
+	if !ok {
+		return "```Could not find " + arg + "!```", false
+	}
+	delete(info.config.Collections[collection], arg)
+	fn, ok := c.funcmap[collection]
+	retval := "```Removed " + arg + " from " + collection + ". Length of " + collection + ": " + strconv.Itoa(len(info.config.Collections[collection])) + "```"
+	if ok {
+		retval = fn(arg)
+	}
+
+	info.SaveConfig()
+	return ExtraSanitize(retval), false
 }
-func (c *RemoveCommand) Usage(info *GuildInfo) string { 
-  return info.FormatUsage(c, "[collection] [arbitrary string]", "Removes [arbitrary string] from [collection] (no quotes are required) and calls a handler function for that collection.") 
+func (c *RemoveCommand) Usage(info *GuildInfo) string {
+	return info.FormatUsage(c, "[collection] [arbitrary string]", "Removes [arbitrary string] from [collection] (no quotes are required) and calls a handler function for that collection.")
 }
 func (c *RemoveCommand) UsageShort() string { return "Removes a line from a collection." }
 
@@ -87,27 +88,27 @@ type CollectionsCommand struct {
 }
 
 func (c *CollectionsCommand) Name() string {
-  return "Collections";  
+	return "Collections"
 }
 func (c *CollectionsCommand) Process(args []string, msg *discordgo.Message, info *GuildInfo) (string, bool) {
-  if len(args) < 1 {
-    s := make([]string, 0, len(info.config.Collections))
-    for k, _ := range info.config.Collections {
-      s = append(s, k)
-    }
-    
-    return "```No collection specified. All collections:\n" + ExtraSanitize(strings.Join(s, "\n")) + "```", false
-  }
+	if len(args) < 1 {
+		s := make([]string, 0, len(info.config.Collections))
+		for k, _ := range info.config.Collections {
+			s = append(s, k)
+		}
 
-  arg := args[0]
-  cmap, ok := info.config.Collections[arg];
-  if !ok {
-    return "```That collection doesn't exist! Use this command with no arguments to see a list of all collections.```", false
-  }
+		return "```No collection specified. All collections:\n" + ExtraSanitize(strings.Join(s, "\n")) + "```", false
+	}
 
-  return "```" + ExtraSanitize(arg + " contains:\n" + strings.Join(MapToSlice(cmap), "\n")) + "```", false
+	arg := args[0]
+	cmap, ok := info.config.Collections[arg]
+	if !ok {
+		return "```That collection doesn't exist! Use this command with no arguments to see a list of all collections.```", false
+	}
+
+	return "```" + ExtraSanitize(arg+" contains:\n"+strings.Join(MapToSlice(cmap), "\n")) + "```", false
 }
-func (c *CollectionsCommand) Usage(info *GuildInfo) string { 
-  return info.FormatUsage(c, "", "Lists all the collections that sweetiebot is using.") 
+func (c *CollectionsCommand) Usage(info *GuildInfo) string {
+	return info.FormatUsage(c, "", "Lists all the collections that sweetiebot is using.")
 }
 func (c *CollectionsCommand) UsageShort() string { return "Lists all collections." }
--- sweetiebot/emote_module.go
+++ sweetiebot/emote_module.go
@@ -1,56 +1,59 @@
 package sweetiebot
 
 import (
-  "github.com/bwmarrin/discordgo"
-  "regexp"
-  "strings"
-  "strconv"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 // The emote module detects banned emotes and deletes them
 type EmoteModule struct {
-  emoteban *regexp.Regexp
-  lastmsg int64
+	emoteban *regexp.Regexp
+	lastmsg  int64
 }
 
 func (w *EmoteModule) Name() string {
-  return "Emote"
+	return "Emote"
 }
 
 func (w *EmoteModule) Register(info *GuildInfo) {
-  w.lastmsg = 0
-  w.UpdateRegex(info)
-  info.hooks.OnMessageCreate = append(info.hooks.OnMessageCreate, w)
-  info.hooks.OnMessageUpdate = append(info.hooks.OnMessageUpdate, w)
-  info.hooks.OnCommand = append(info.hooks.OnCommand, w)
+	w.lastmsg = 0
+	w.UpdateRegex(info)
+	info.hooks.OnMessageCreate = append(info.hooks.OnMessageCreate, w)
+	info.hooks.OnMessageUpdate = append(info.hooks.OnMessageUpdate, w)
+	info.hooks.OnCommand = append(info.hooks.OnCommand, w)
 }
 
 func (w *EmoteModule) HasBigEmote(info *GuildInfo, m *discordgo.Message) bool {
-  if w.emoteban.MatchString(m.Content) {
-    sb.dg.ChannelMessageDelete(m.ChannelID, m.ID)
-    if RateLimit(&w.lastmsg, 5) {
-      info.SendMessage(m.ChannelID, "`That emote isn't allowed here! Try to avoid using large or disturbing emotes, as they can be problematic.`")
-    }
-    return true
-  }
-  return false
+	if w.emoteban.MatchString(m.Content) {
+		sb.dg.ChannelMessageDelete(m.ChannelID, m.ID)
+		if RateLimit(&w.lastmsg, 5) {
+			info.SendMessage(m.ChannelID, "`That emote isn't allowed here! Try to avoid using large or disturbing emotes, as they can be problematic.`")
+		}
+		return true
+	}
+	return false
 }
 
 func (w *EmoteModule) OnMessageCreate(info *GuildInfo, m *discordgo.Message) {
-  w.HasBigEmote(info, m)
+	w.HasBigEmote(info, m)
 }
-  
+
 func (w *EmoteModule) OnMessageUpdate(info *GuildInfo, m *discordgo.Message) {
-  w.HasBigEmote(info, m)
+	w.HasBigEmote(info, m)
 }
 
 func (w *EmoteModule) OnCommand(info *GuildInfo, m *discordgo.Message) bool {
-  if info.UserHasRole(m.Author.ID, strconv.FormatUint(info.config.AlertRole, 10)) { return false }
-  return w.HasBigEmote(info, m)
+	if info.UserHasRole(m.Author.ID, strconv.FormatUint(info.config.AlertRole, 10)) {
+		return false
+	}
+	return w.HasBigEmote(info, m)
 }
 
 func (w *EmoteModule) UpdateRegex(info *GuildInfo) bool {
-  var err error
-  w.emoteban, err = regexp.Compile("\\[\\]\\(\\/r?(" + strings.Join(MapToSlice(info.config.Collections["emote"]), "|") + ")[-) \"]")
-  return err == nil
+	var err error
+	w.emoteban, err = regexp.Compile("\\[\\]\\(\\/r?(" + strings.Join(MapToSlice(info.config.Collections["emote"]), "|") + ")[-) \"]")
+	return err == nil
 }
