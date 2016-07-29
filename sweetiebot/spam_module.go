8eda42c8ce2b93523f5a23604acf8c989f50cf31
Erik McClure <erikm@fundament.software>
2016-07-29 01:10:16 -0700

    Added disablebored for partially disabling bored commands.

--- sweetiebot/spam_module.go
+++ sweetiebot/spam_module.go
@@ -1,99 +1,101 @@
 package sweetiebot
 
 import (
-  "github.com/bwmarrin/discordgo"
-  "time"
-  "strconv"
-  "strings"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 // The emote module detects banned emotes and deletes them
 type SpamModule struct {
-  tracker map[uint64]*SaturationLimit
-  lastraid int64
+	tracker  map[uint64]*SaturationLimit
+	lastraid int64
 }
 
 func (w *SpamModule) Name() string {
-  return "Anti-Spam"
+	return "Anti-Spam"
 }
 
 func (w *SpamModule) Register(info *GuildInfo) {
-  w.tracker = make(map[uint64]*SaturationLimit)
-  w.lastraid = 0
-  info.hooks.OnMessageCreate = append(info.hooks.OnMessageCreate, w)
-  info.hooks.OnCommand = append(info.hooks.OnCommand, w)
-  info.hooks.OnGuildMemberAdd = append(info.hooks.OnGuildMemberAdd, w)
-  info.hooks.OnGuildMemberUpdate = append(info.hooks.OnGuildMemberUpdate, w)
+	w.tracker = make(map[uint64]*SaturationLimit)
+	w.lastraid = 0
+	info.hooks.OnMessageCreate = append(info.hooks.OnMessageCreate, w)
+	info.hooks.OnCommand = append(info.hooks.OnCommand, w)
+	info.hooks.OnGuildMemberAdd = append(info.hooks.OnGuildMemberAdd, w)
+	info.hooks.OnGuildMemberUpdate = append(info.hooks.OnGuildMemberUpdate, w)
 }
 
-func KillSpammer(u *discordgo.User, info *GuildInfo) {  
-  // Manually set our internal state to say this user has the Silent role, to prevent race conditions
-  m, err := sb.dg.State.Member(info.Guild.ID, u.ID)
-  if err == nil {
-    srole := strconv.FormatUint(info.config.SilentRole, 10)
-    for _, v := range m.Roles {
-      if v == srole {
-        return // Spammer was already killed, so don't try killing it again
-      }
-    }
-    m.Roles = append(m.Roles, srole)
-  } else {
-    info.log.Log("Tried to kill spammer ", u.Username, " but they were already banned??? (Error: ", err.Error(), ")")
-    return
-  }
-  
-  info.log.Log("Killing spammer ", u.Username)
-  
-  sb.dg.GuildMemberEdit(info.Guild.ID, u.ID, m.Roles) // Tell discord to make this spammer silent
-  messages := sb.db.GetRecentMessages(SBatoi(u.ID), 60) // Retrieve all messages in the past 60 seconds and delete them.
+func KillSpammer(u *discordgo.User, info *GuildInfo) {
+	// Manually set our internal state to say this user has the Silent role, to prevent race conditions
+	m, err := sb.dg.State.Member(info.Guild.ID, u.ID)
+	if err == nil {
+		srole := strconv.FormatUint(info.config.SilentRole, 10)
+		for _, v := range m.Roles {
+			if v == srole {
+				return // Spammer was already killed, so don't try killing it again
+			}
+		}
+		m.Roles = append(m.Roles, srole)
+	} else {
+		info.log.Log("Tried to kill spammer ", u.Username, " but they were already banned??? (Error: ", err.Error(), ")")
+		return
+	}
 
-  for _, v := range messages {
-    sb.dg.ChannelMessageDelete(strconv.FormatUint(v.channel, 10), strconv.FormatUint(v.message, 10))
-  }
-  
-  info.SendMessage(strconv.FormatUint(info.config.ModChannel, 10), "`Alert: " + u.Username + " was silenced for spamming. Please investigate.`") // Alert admins
+	info.log.Log("Killing spammer ", u.Username)
+
+	sb.dg.GuildMemberEdit(info.Guild.ID, u.ID, m.Roles)   // Tell discord to make this spammer silent
+	messages := sb.db.GetRecentMessages(SBatoi(u.ID), 60) // Retrieve all messages in the past 60 seconds and delete them.
+
+	for _, v := range messages {
+		sb.dg.ChannelMessageDelete(strconv.FormatUint(v.channel, 10), strconv.FormatUint(v.message, 10))
+	}
+
+	info.SendMessage(strconv.FormatUint(info.config.ModChannel, 10), "`Alert: "+u.Username+" was silenced for spamming. Please investigate.`") // Alert admins
 }
 func (w *SpamModule) CheckSpam(info *GuildInfo, m *discordgo.Message) bool {
-  if m.Author != nil {
-    if info.UserHasRole(m.Author.ID, strconv.FormatUint(info.config.SilentRole, 10)) {
-      sb.dg.ChannelMessageDelete(m.ChannelID, m.ID);
-      return true
-    }
-    id := SBatoi(m.Author.ID)
-    _, ok := w.tracker[id]
-    if !ok {
-      w.tracker[id] = &SaturationLimit{make([]int64, info.config.Maxspam, info.config.Maxspam), 0, AtomicFlag{0}};
-    }
-    limit := w.tracker[id]
-    limit.append(time.Now().UTC().Unix())
-    if limit.checkafter(5, 1) || limit.checkafter(7, 4) || limit.checkafter(10, 9) {
-      KillSpammer(m.Author, info)
-      return true
-    }
-  }
-  return false
+	if m.Author != nil {
+		if info.UserHasRole(m.Author.ID, strconv.FormatUint(info.config.SilentRole, 10)) {
+			sb.dg.ChannelMessageDelete(m.ChannelID, m.ID)
+			return true
+		}
+		id := SBatoi(m.Author.ID)
+		_, ok := w.tracker[id]
+		if !ok {
+			w.tracker[id] = &SaturationLimit{make([]int64, 20, 20), 0, AtomicFlag{0}}
+		}
+		limit := w.tracker[id]
+		limit.append(time.Now().UTC().Unix())
+		if limit.checkafter(5, 1) || limit.checkafter(7, 4) || limit.checkafter(10, 9) {
+			KillSpammer(m.Author, info)
+			return true
+		}
+	}
+	return false
 }
 func (w *SpamModule) OnMessageCreate(info *GuildInfo, m *discordgo.Message) {
-  w.CheckSpam(info, m)
+	w.CheckSpam(info, m)
 }
 func (w *SpamModule) OnCommand(info *GuildInfo, m *discordgo.Message) bool {
-  return w.CheckSpam(info, m)
+	return w.CheckSpam(info, m)
 }
 func (w *SpamModule) OnGuildMemberAdd(info *GuildInfo, m *discordgo.Member) {
-  raidsize := sb.db.CountNewUsers(info.config.MaxRaidTime, SBatoi(info.Guild.ID));
-  if info.config.RaidSize > 0 && raidsize >= info.config.RaidSize && RateLimit(&w.lastraid, info.config.MaxRaidTime*2) {  
-    r := sb.db.GetNewestUsers(raidsize, SBatoi(info.Guild.ID))
-    s := make([]string, 0, len(r))
-    
-    for _, v := range r {
-      s = append(s, v.Username + "  (joined: " + v.FirstSeen.Format(time.ANSIC) + ")") 
-    }
-    ch := strconv.FormatUint(info.config.ModChannel, 10)
-    if info.config.Debug { ch, _ = sb.DebugChannels[info.Guild.ID] }
-    info.SendMessage(ch, "<@&" + strconv.FormatUint(info.config.AlertRole, 10) + "> Possible Raid Detected!\n```" + strings.Join(s, "\n") + "```")
-  }
+	raidsize := sb.db.CountNewUsers(info.config.MaxRaidTime, SBatoi(info.Guild.ID))
+	if info.config.RaidSize > 0 && raidsize >= info.config.RaidSize && RateLimit(&w.lastraid, info.config.MaxRaidTime*2) {
+		r := sb.db.GetNewestUsers(raidsize, SBatoi(info.Guild.ID))
+		s := make([]string, 0, len(r))
+
+		for _, v := range r {
+			s = append(s, v.Username+"  (joined: "+v.FirstSeen.Format(time.ANSIC)+")")
+		}
+		ch := strconv.FormatUint(info.config.ModChannel, 10)
+		if info.config.Debug {
+			ch, _ = sb.DebugChannels[info.Guild.ID]
+		}
+		info.SendMessage(ch, "<@&"+strconv.FormatUint(info.config.AlertRole, 10)+"> Possible Raid Detected!\n```"+strings.Join(s, "\n")+"```")
+	}
 }
 func (w *SpamModule) OnGuildMemberUpdate(info *GuildInfo, m *discordgo.Member) {
-  w.OnGuildMemberAdd(info, m)
+	w.OnGuildMemberAdd(info, m)
 }
-  
--- sweetiebot/spoiler_module.go
+++ sweetiebot/spoiler_module.go
@@ -1,66 +1,69 @@
 package sweetiebot
 
 import (
-  "github.com/bwmarrin/discordgo"
-  "strings"
-  "strconv"
-  "regexp"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
 )
 
 // This module picks a random action to do whenever #example has been idle for several minutes (configurable)
 type SpoilerModule struct {
-  spoilerban *regexp.Regexp
-  lastmsg int64 // Sanity rate limiter
+	spoilerban *regexp.Regexp
+	lastmsg    int64 // Sanity rate limiter
 }
 
 func (w *SpoilerModule) Name() string {
-  return "Spoiler"
+	return "Spoiler"
 }
 
 func (w *SpoilerModule) Register(info *GuildInfo) {
-  w.lastmsg = 0
-  w.UpdateRegex(info)
-  info.hooks.OnMessageCreate = append(info.hooks.OnMessageCreate, w)
-  info.hooks.OnMessageUpdate = append(info.hooks.OnMessageUpdate, w)
-  info.hooks.OnCommand = append(info.hooks.OnCommand, w)
+	w.lastmsg = 0
+	w.UpdateRegex(info)
+	info.hooks.OnMessageCreate = append(info.hooks.OnMessageCreate, w)
+	info.hooks.OnMessageUpdate = append(info.hooks.OnMessageUpdate, w)
+	info.hooks.OnCommand = append(info.hooks.OnCommand, w)
 }
 
 func (w *SpoilerModule) HasSpoiler(info *GuildInfo, m *discordgo.Message) bool {
-  cid := SBatoi(m.ChannelID)
-  for _, v := range info.config.SpoilChannels {
-    if cid == v {
-      return false // this is a spoiler channel so we don't monitor it
-    }
-  }
-  if w.spoilerban != nil && w.spoilerban.MatchString(strings.ToLower(m.Content)) {
-    sb.dg.ChannelMessageDelete(m.ChannelID, m.ID)
-    if RateLimit(&w.lastmsg, info.config.Maxspoiltime) {
-      info.SendMessage(m.ChannelID, "[](/nospoilers) ```NO SPOILERS! Posting spoilers is a bannable offense. All discussion about new and future content MUST be in #mylittlespoilers.```")
-    }
-    return true
-  }
-  return false
+	cid := SBatoi(m.ChannelID)
+	for _, v := range info.config.SpoilChannels {
+		if cid == v {
+			return false // this is a spoiler channel so we don't monitor it
+		}
+	}
+	if w.spoilerban != nil && w.spoilerban.MatchString(strings.ToLower(m.Content)) {
+		sb.dg.ChannelMessageDelete(m.ChannelID, m.ID)
+		if RateLimit(&w.lastmsg, info.config.Maxerror) {
+			info.SendMessage(m.ChannelID, "[](/nospoilers) ```NO SPOILERS! Posting spoilers is a bannable offense. All discussion about new and future content MUST be in #mylittlespoilers.```")
+		}
+		return true
+	}
+	return false
 }
 
 func (w *SpoilerModule) OnMessageCreate(info *GuildInfo, m *discordgo.Message) {
-  w.HasSpoiler(info, m)
+	w.HasSpoiler(info, m)
 }
-  
+
 func (w *SpoilerModule) OnMessageUpdate(info *GuildInfo, m *discordgo.Message) {
-  w.HasSpoiler(info, m)
+	w.HasSpoiler(info, m)
 }
 
 func (w *SpoilerModule) OnCommand(info *GuildInfo, m *discordgo.Message) bool {
-  if info.UserHasRole(m.Author.ID, strconv.FormatUint(info.config.AlertRole, 10)) { return false } // If we are a princess, always allow us to run this command, otherwise we can't unspoil things
-  return w.HasSpoiler(info, m)
+	if info.UserHasRole(m.Author.ID, strconv.FormatUint(info.config.AlertRole, 10)) {
+		return false
+	} // If we are a princess, always allow us to run this command, otherwise we can't unspoil things
+	return w.HasSpoiler(info, m)
 }
 
 func (w *SpoilerModule) UpdateRegex(info *GuildInfo) bool {
-  if len(info.config.Collections["spoiler"]) < 1 {
-    w.spoilerban = nil
-    return true
-  }
-  var err error
-  w.spoilerban, err = regexp.Compile("(" + strings.Join(MapToSlice(info.config.Collections["spoiler"]), "|") + ")")
-  return err == nil
+	if len(info.config.Collections["spoiler"]) < 1 {
+		w.spoilerban = nil
+		return true
+	}
+	var err error
+	w.spoilerban, err = regexp.Compile("(" + strings.Join(MapToSlice(info.config.Collections["spoiler"]), "|") + ")")
+	return err == nil
 }
