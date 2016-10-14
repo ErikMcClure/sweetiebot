36162f53502b1d00cf2cc74c8cf5de3fd87988a3
Erik McClure <erikm@blackspherestudios.com>
2016-10-14 01:37:06 -0700

    Added !poll, !vote, !createpoll, !deletepoll and !results commands

--- sweetiebot/sweetiebot.go
+++ sweetiebot/sweetiebot.go
@@ -584,6 +584,11 @@ func AttachToGuild(g *discordgo.Guild) {
 	guild.AddCommand(&DefaultServerCommand{})
 	guild.AddCommand(&RulesCommand{})
 	guild.AddCommand(&ChangelogCommand{})
+	guild.AddCommand(&PollCommand{})
+	guild.AddCommand(&CreatePollCommand{})
+	guild.AddCommand(&DeletePollCommand{})
+	guild.AddCommand(&VoteCommand{})
+	guild.AddCommand(&ResultsCommand{})
 
 	if disableall {
 		for k, _ := range guild.commands {
@@ -1127,7 +1132,7 @@ func Initialize(Token string) {
 	rand.Seed(time.Now().UTC().Unix())
 
 	sb = &SweetieBot{
-		version:            Version{0, 8, 13, 1},
+		version:            Version{0, 8, 14, 0},
 		Debug:              (err == nil && len(isdebug) > 0),
 		Owners:             map[uint64]bool{95585199324143616: true, 98605232707080192: true},
 		RestrictedCommands: map[string]bool{"search": true, "lastping": true, "setstatus": true},
@@ -1141,6 +1146,7 @@ func Initialize(Token string) {
 		LastMessages:       make(map[string]int64),
 		MaxConfigSize:      1000000,
 		changelog: map[int]string{
+			AssembleVersion(0, 8, 14, 0): "- Added !poll, !vote, !createpoll, !deletepoll and !results commands",
 			AssembleVersion(0, 8, 13, 1): "- Fixed !setconfig rules",
 			AssembleVersion(0, 8, 13, 0): "- Added changelog\n- Added !rules command",
 			AssembleVersion(0, 8, 12, 0): "- Added temporary silences",
