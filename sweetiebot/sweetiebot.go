723bf41640c9ed852a21c72c5c81fed76cd8c710
Erik McClure <erikm@blackspherestudios.com>
2016-10-18 01:23:34 -0700

    god damn it tawmy

--- sweetiebot/sweetiebot.go
+++ sweetiebot/sweetiebot.go
@@ -1135,7 +1135,7 @@ func Initialize(Token string) {
 	rand.Seed(time.Now().UTC().Unix())
 
 	sb = &SweetieBot{
-		version:            Version{0, 8, 14, 4},
+		version:            Version{0, 8, 14, 5},
 		Debug:              (err == nil && len(isdebug) > 0),
 		Owners:             map[uint64]bool{95585199324143616: true, 98605232707080192: true},
 		RestrictedCommands: map[string]bool{"search": true, "lastping": true, "setstatus": true},
@@ -1149,6 +1149,7 @@ func Initialize(Token string) {
 		LastMessages:       make(map[string]int64),
 		MaxConfigSize:      1000000,
 		changelog: map[int]string{
+			AssembleVersion(0, 8, 14, 5): "- Allow exact string matching on !import",
 			AssembleVersion(0, 8, 14, 4): "- Added !import\n- Added Importable option\n- Make !collections more useful",
 			AssembleVersion(0, 8, 14, 3): "- Allow pinging multiple groups via group1+group2",
 			AssembleVersion(0, 8, 14, 2): "- Fix !createpoll unique option key\n- Add !addoption",
