94f26e4b9dbd34a967bd49ff3d10b695931d6d02
Tyler <supersnapple@gmail.com>
2020-12-29 06:58:56 -0600

    Bumping version

--- sweetiebot/sweetiebot.go
+++ sweetiebot/sweetiebot.go
@@ -36,7 +36,7 @@ var guildfileregex = regexp.MustCompile("^([0-9]+)[.]json$")
 const DiscordEpoch uint64 = 1420070400000
 
 // BotVersion stores the current version of sweetiebot
-var BotVersion = Version{1, 0, 1, 8}
+var BotVersion = Version{1, 0, 1, 9}
 
 const (
 	MaxPublicLines    = 12
